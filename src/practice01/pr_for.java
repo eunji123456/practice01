dc32f4f8a2481aee19b8b35a9684b4fa3fdc47fd
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-05 11:32:21 +0900

    반복문 문제 1 번

---
+++ src/practice01/pr_for.java
@@ -0,0 +1,14 @@
+package practice01;
+
+public class pr_for {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int a=1;a<=100;a++) {
+			if(a%5==0&&a%7==0) {
+				System.out.println(a);
+			}
+		}
+	}
+
+}
